cd76edb0ee909bc28d51bb661ff40492d71d5564
yasin-yumrutas <yasinyumrutas0@gmail.com>
2025-03-05 03:59:09 +0300

    Burada partner için yapılabilicek bir animasyon var ~Yasin Yumrutaş

---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import "net/http"
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "index.html")
+	})
+
+	http.ListenAndServe(":9999", nil)
+}
